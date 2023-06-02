229c3de1e956ea9992165ac0b7e14f1ba57ad886
alvesgeraldo <jgalvesb@gmail.com>
2023-06-02 16:59:39 -0300

    Introduçao as PSR com PHP Codesniffer

--- src/Search.php
+++ src/Search.php
@@ -2,26 +2,23 @@
 
   namespace Geraldo\CepDigital;
 
-  class Search
-  {
-
+class Search
+{
     private $url = "https://viacep.com.br/ws/";
 
-    public function getAddress(string $zipCode):array
+    public function getAddress(string $zipCode): array
     {
-      
-      $get = file_get_contents($this->setZipCode($zipCode));
 
-      return (array) json_decode($get);
-     
+        $get = file_get_contents($this->setZipCode($zipCode));
+
+        return (array) json_decode($get);
     }
 
     public function setZipCode(string $zipCode)
     {
 
-      $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-      return $this->url . $zipCode . '/json';
-
+        return $this->url . $zipCode . '/json';
     }
-  }
+}
